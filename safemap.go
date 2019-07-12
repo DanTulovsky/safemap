69ce7e70c880efa94ca94903393d2c6851c1f331
Dan Tulovsky <dant@wetsnow.com>
2019-07-12 17:31:31 -0400

    updates

--- safemap.go
+++ safemap.go
@@ -3,23 +3,24 @@ package safemap
 
 import deadlock "github.com/sasha-s/go-deadlock"
 
-type safeMap struct {
+// SafeMap provides a map safe for concurrent updates
+type SafeMap struct {
 	deadlock.RWMutex
 	data map[string]interface{}
 }
 
-type safeMapItem struct {
+type SafeMapItem struct {
 	Key   string
 	Value interface{}
 }
 
-func NewSafeMap() *safeMap {
-	return &safeMap{
+func NewSafeMap() *SafeMap {
+	return &SafeMap{
 		data: make(map[string]interface{}),
 	}
 }
 
-func (sm *safeMap) Keys() []string {
+func (sm *SafeMap) Keys() []string {
 	sm.RLock()
 	defer sm.RUnlock()
 
@@ -30,14 +31,14 @@ func (sm *safeMap) Keys() []string {
 	return keys
 }
 
-func (sm *safeMap) Iter() <-chan safeMapItem {
-	c := make(chan safeMapItem, 10)
+func (sm *SafeMap) Iter() <-chan SafeMapItem {
+	c := make(chan SafeMapItem, 10)
 
 	f := func() {
 		sm.RLock()
 		defer sm.RUnlock()
 		for k, v := range sm.data {
-			c <- safeMapItem{k, v}
+			c <- SafeMapItem{k, v}
 		}
 		close(c)
 	}
@@ -46,32 +47,32 @@ func (sm *safeMap) Iter() <-chan safeMapItem {
 	return c
 }
 
-func (sm *safeMap) Insert(key string, value interface{}) {
+func (sm *SafeMap) Insert(key string, value interface{}) {
 	sm.Lock()
 	defer sm.Unlock()
 	sm.data[key] = value
 }
 
-func (sm *safeMap) Delete(key string) {
+func (sm *SafeMap) Delete(key string) {
 	sm.Lock()
 	defer sm.Unlock()
 	delete(sm.data, key)
 }
 
-func (sm *safeMap) Find(key string) (interface{}, bool) {
+func (sm *SafeMap) Find(key string) (interface{}, bool) {
 	sm.RLock()
 	defer sm.RUnlock()
 	v, ok := sm.data[key]
 	return v, ok
 }
 
-func (sm *safeMap) Len() int {
+func (sm *SafeMap) Len() int {
 	sm.RLock()
 	defer sm.RUnlock()
 	return len(sm.data)
 }
 
-func (sm *safeMap) Update(key string, value interface{}) {
+func (sm *SafeMap) Update(key string, value interface{}) {
 	sm.Lock()
 	defer sm.Unlock()
 	sm.data[key] = value
